279859793edb99ac89251f4dc33d820d97bfc2c8
Wafi Faisal Falah <wafifaisal19@gmail.com>
2024-10-03 17:39:03 +0700

    exercise day-1

--- exercise_01/string.ts
+++ exercise_01/string.ts
@@ -1,16 +1,16 @@
 let pesan: string = "Hello World"
 let pesanUpper: string = pesan.toUpperCase()
-console.log(pesanUpper);
+//console.log(pesanUpper);
 // console.log(pesan.toUpperCase())
 // console.log(pesan.toLowerCase())
 // console.log(pesan.replace("e","a"))
-// console.log(pesan.slice(1, 5)) // ngitung kata dari 0
-// console.log(pesan.charAt(5))
+console.log(pesan.slice(1, 5)) // ngitung kata dari 0
+//console.log(pesan.charAt(5))
 
-console.log(pesan.toLocaleUpperCase().replace("o","a")) // Ini yang salah
-console.log(pesan.toLocaleUpperCase().replace("O","A")) // Ini yang benar
+//console.log(pesan.toLocaleUpperCase().replace("o","a")) // Ini yang salah
+//console.log(pesan.toLocaleUpperCase().replace("O","A")) // Ini yang benar
 
-console.log(pesan.toLocaleUpperCase().replace("O","A").replace("O","A")) // Ini yang benar
+//console.log(pesan.toLocaleUpperCase().replace("O","A").replace("O","A")) // Ini yang benar
 
 console.log(pesan.concat(pesanUpper)); // Ini yang benar
 
